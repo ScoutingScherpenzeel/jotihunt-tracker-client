0e7eb97cdc283a2f847660b5050e1f6c049f2790
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2024-10-04 19:17:25 +0200

    feat: Improved PWA handling

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,48 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { viteEnvs } from 'vite-envs';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const pwaManifest: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['icon512_maskable.png', 'favicon.ico'],
+  devOptions: {
+    enabled: true,
+  },
+  manifest: {
+    name: 'Jotihunt Tracker',
+    short_name: 'Jotihunt Tracker',
+    description: 'Jotihunt Tracker voor Scouting Scherpenzeel e.o.',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'icon192_maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'icon512_maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    screenshots: [
+      {
+        src: 'screenshot_narrow.png',
+        sizes: '900x1600',
+        type: 'image/png',
+        form_factor: 'narrow',
+      },
+      {
+        src: 'screenshot_wide.png',
+        sizes: '3800x2136',
+        type: 'image/png',
+        form_factor: 'wide',
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +52,8 @@ export default defineConfig({
     viteEnvs({
       declarationFile: '.env.example',
     }),
+    VitePWA(pwaManifest),
+    basicSsl(),
   ],
   resolve: {
     alias: {
