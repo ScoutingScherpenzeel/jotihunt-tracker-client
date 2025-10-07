9ef657fed35319c01cc6336fa701b2a40865dbfb
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2025-10-07 18:02:40 +0200

    feat: Added support for the new areas

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import {type ClassValue, clsx} from 'clsx';
+import {twMerge} from 'tailwind-merge';
 import * as turf from '@turf/turf';
 
 /**
@@ -8,7 +8,7 @@ import * as turf from '@turf/turf';
  * @returns The className string
  */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 /**
@@ -17,7 +17,7 @@ export function cn(...inputs: ClassValue[]) {
  * @returns The converted value in km/h
  */
 export function knotsToKmh(knots: number) {
-  return knots * 1.852;
+    return knots * 1.852;
 }
 
 /**
@@ -26,23 +26,27 @@ export function knotsToKmh(knots: number) {
  * @returns The color in hex format
  */
 export function getColorFromArea(area: string) {
-  area = area.trim().toLowerCase();
-  switch (area) {
-    case 'alpha':
-      return '#f87171';
-    case 'bravo':
-      return '#4ade80';
-    case 'charlie':
-      return '#60a5fa';
-    case 'delta':
-      return '#facc15';
-    case 'echo':
-      return '#e879f9';
-    case 'foxtrot':
-      return '#2dd4bf';
-    default:
-      return '#9ca3af';
-  }
+    area = area.trim().toLowerCase();
+    switch (area) {
+        case 'alpha':
+            return '#f87171';
+        case 'bravo':
+            return '#4ade80';
+        case 'charlie':
+            return '#60a5fa';
+        case 'delta':
+            return '#facc15';
+        case 'echo':
+            return '#e879f9';
+        case 'foxtrot':
+            return '#2dd4bf';
+        case 'golf':
+            return '#fa9b60';
+        case 'hotel':
+            return '#6d45fd';
+        default:
+            return '#9ca3af';
+    }
 }
 
 /**
@@ -51,7 +55,7 @@ export function getColorFromArea(area: string) {
  * @returns The capitalized string
  */
 export function capitalizeFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 /**
@@ -62,12 +66,11 @@ export function capitalizeFirstLetter(string: string) {
  * @returns The circle in turf.js format
  */
 export function createCircle(longitude: number, latitude: number, radius: number) {
-  const center = [longitude, latitude];
-  const circle = turf.circle(center, radius, {
-    steps: 64,
-    units: 'meters',
-  });
-  return circle;
+    const center = [longitude, latitude];
+    return turf.circle(center, radius, {
+        steps: 64,
+        units: 'meters',
+    });
 }
 
 /**
@@ -80,30 +83,30 @@ export function createCircle(longitude: number, latitude: number, radius: number
  * @returns The wind sector in turf.js format
  */
 export function createWindSector(longitude: number, latitude: number, radius: number, direction: number, angle: number) {
-  const center = [longitude, latitude];
-  const halfAngle = angle / 2;
+    const center = [longitude, latitude];
+    const halfAngle = angle / 2;
 
-  // Start and end angles for the sector
-  const startAngle = direction - halfAngle;
-  const endAngle = direction + halfAngle;
+    // Start and end angles for the sector
+    const startAngle = direction - halfAngle;
+    const endAngle = direction + halfAngle;
 
-  // Generate points along the arc
-  const arc = turf.lineArc(center, radius, startAngle, endAngle, {
-    steps: 64,
-    units: 'meters',
-  });
+    // Generate points along the arc
+    const arc = turf.lineArc(center, radius, startAngle, endAngle, {
+        steps: 64,
+        units: 'meters',
+    });
 
-  // Create a polygon by closing the arc
-  const sector = turf.polygon([[center, ...arc.geometry.coordinates, center]]);
-
-  return sector;
+    // Create a polygon by closing the arc
+    return turf.polygon([[center, ...arc.geometry.coordinates, center]]);
 }
 
 export const areaOptions = [
-  { value: 'alpha', label: 'Alpha' },
-  { value: 'bravo', label: 'Bravo' },
-  { value: 'charlie', label: 'Charlie' },
-  { value: 'delta', label: 'Delta' },
-  { value: 'echo', label: 'Echo' },
-  { value: 'foxtrot', label: 'Foxtrot' },
+    {value: 'alpha', label: 'Alpha'},
+    {value: 'bravo', label: 'Bravo'},
+    {value: 'charlie', label: 'Charlie'},
+    {value: 'delta', label: 'Delta'},
+    {value: 'echo', label: 'Echo'},
+    {value: 'foxtrot', label: 'Foxtrot'},
+    {value: 'golf', label: 'Golf'},
+    {value: 'hotel', label: 'Hotel'}
 ];
