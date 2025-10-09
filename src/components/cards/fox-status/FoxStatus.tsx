cb48d2b8cecf49eb85da89f271a14931e2bd2382
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2025-10-09 20:32:00 +0200

    feat: Added color indicator for fox status card

--- src/components/cards/fox-status/FoxStatus.tsx
+++ src/components/cards/fox-status/FoxStatus.tsx
@@ -1,112 +1,122 @@
-import PropTypes, { InferProps } from 'prop-types';
-import { Tooltip, TooltipContent, TooltipTrigger } from '../../ui/tooltip.tsx';
-import { addSeconds, differenceInSeconds, formatDistance, formatDistanceToNow, isBefore } from 'date-fns';
-import { nl } from 'date-fns/locale';
-import { Hunt } from '@/types/Hunt.ts';
-import { useEffect, useState } from 'react';
+import PropTypes, {InferProps} from 'prop-types';
+import {Tooltip, TooltipContent, TooltipTrigger} from '../../ui/tooltip.tsx';
+import {addSeconds, differenceInSeconds, formatDistance, formatDistanceToNow, isBefore} from 'date-fns';
+import {nl} from 'date-fns/locale';
+import {Hunt} from '@/types/Hunt.ts';
+import {useEffect, useState} from 'react';
+import {getColorFromArea} from "@/lib/utils.ts";
 
-export default function FoxStatus({ name, status, lastUpdate, lastHunt, hidden }: InferProps<typeof FoxStatus.propTypes>) {
-  const [currentTime, setCurrentTime] = useState(new Date());
+export default function FoxStatus({
+                                      name,
+                                      status,
+                                      lastUpdate,
+                                      lastHunt,
+                                      hidden
+                                  }: InferProps<typeof FoxStatus.propTypes>) {
+    const [currentTime, setCurrentTime] = useState(new Date());
 
-  /**
-   * Convert the status of an area to a tailwind class.
-   * @param status The status of the area
-   * @returns The tailwind classes for the status
-   */
-  function getStatusStyles(status: string) {
-    const isHuntable = areaIsHuntable();
-    if (!isHuntable) return 'bg-blue-200 border-blue-400 text-blue-600';
-    switch (status) {
-      case 'orange':
-        return 'bg-orange-200 border-orange-400 text-orange-600';
-      case 'red':
-        return 'bg-red-200 border-red-400 text-red-600';
-      case 'green':
-        return 'bg-green-200 border-green-400 text-green-600';
-      default:
-        return 'bg-gray-200 border-gray-400 text-gray-600';
+    /**
+     * Convert the status of an area to a tailwind class.
+     * @param status The status of the area
+     * @returns The tailwind classes for the status
+     */
+    function getStatusStyles(status: string) {
+        const isHuntable = areaIsHuntable();
+        if (!isHuntable) return 'bg-blue-200 border-blue-400 text-blue-600';
+        switch (status) {
+            case 'orange':
+                return 'bg-orange-200 border-orange-400 text-orange-600';
+            case 'red':
+                return 'bg-red-200 border-red-400 text-red-600';
+            case 'green':
+                return 'bg-green-200 border-green-400 text-green-600';
+            default:
+                return 'bg-gray-200 border-gray-400 text-gray-600';
+        }
     }
-  }
 
-  /**
-   * Whether the area is huntable or not.
-   * @returns True if the area is huntable, false otherwise
-   */
-  function areaIsHuntable() {
-    if (!lastHunt || !lastHunt.huntTime) return true;
-    const lastHuntTime = new Date(lastHunt.huntTime);
-    const nextHuntTime = addSeconds(lastHuntTime, 3600); // 3600 seconds = 1 hour
-    return isBefore(nextHuntTime, currentTime); // Checks if next hunt time is before the current time
-  }
+    /**
+     * Whether the area is huntable or not.
+     * @returns True if the area is huntable, false otherwise
+     */
+    function areaIsHuntable() {
+        if (!lastHunt || !lastHunt.huntTime) return true;
+        const lastHuntTime = new Date(lastHunt.huntTime);
+        const nextHuntTime = addSeconds(lastHuntTime, 3600); // 3600 seconds = 1 hour
+        return isBefore(nextHuntTime, currentTime); // Checks if next hunt time is before the current time
+    }
 
-  /**
-   * The time until the area is huntable again (after 1h)
-   */
-  function timeUntilHuntable() {
-    if (!lastHunt || !lastHunt.huntTime) return null;
-    const nextHuntTime = addSeconds(lastHunt.huntTime, 3600); // 3600 seconds = 1 hour
-    if (currentTime >= nextHuntTime) return null;
+    /**
+     * The time until the area is huntable again (after 1h)
+     */
+    function timeUntilHuntable() {
+        if (!lastHunt || !lastHunt.huntTime) return null;
+        const nextHuntTime = addSeconds(lastHunt.huntTime, 3600); // 3600 seconds = 1 hour
+        if (currentTime >= nextHuntTime) return null;
 
-    const secondsDifference = differenceInSeconds(nextHuntTime, currentTime);
-    const hours = Math.floor(secondsDifference / 3600);
-    const minutes = Math.floor((secondsDifference % 3600) / 60);
-    const seconds = secondsDifference % 60;
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }
+        const secondsDifference = differenceInSeconds(nextHuntTime, currentTime);
+        const hours = Math.floor(secondsDifference / 3600);
+        const minutes = Math.floor((secondsDifference % 3600) / 60);
+        const seconds = secondsDifference % 60;
+        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    }
 
-  /**
-   * Run a timer every second to update the shown timer.
-   */
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
+    /**
+     * Run a timer every second to update the shown timer.
+     */
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+        return () => clearInterval(intervalId);
+    }, []);
 
-  return (
-    <>
-        <Tooltip delayDuration={300}>
-          <TooltipTrigger className="w-full">
-            <div className={`border-2 rounded-lg h-[52px] flex flex-col justify-center leading-none hover:brightness-105 ${hidden && 'opacity-50'} ${getStatusStyles(status)}`}>
-              {areaIsHuntable() ? (
-                name
-              ) : (
-                <>
-                  <p className="text-xs">{name}</p>
-                  <p>{timeUntilHuntable()}</p>
-                </>
-              )}
-            </div>
-          </TooltipTrigger>
-          <TooltipContent className="hidden md:block">
-            <p>
-              Status laatst bijgewerkt:{' '}
-              {formatDistanceToNow(new Date(lastUpdate), {
-                locale: nl,
-                addSuffix: true,
-              })}
-            </p>
-            <p>
-              Laatste hunt:{' '}
-              {lastHunt
-                ? formatDistance(new Date(lastHunt.huntTime), new Date(), {
-                    locale: nl,
-                    addSuffix: true,
-                  })
-                : 'Nog geen'}
-            </p>
-          </TooltipContent>
-        </Tooltip>
-    </>
-  );
+    return (
+        <>
+            <Tooltip delayDuration={300}>
+                <TooltipTrigger className="w-full">
+                    <div
+                        className={`border-2 rounded-lg h-[52px] flex flex-col justify-center leading-none hover:brightness-105 relative ${hidden && 'opacity-50'} ${getStatusStyles(status)}`}>
+                        <span style={{backgroundColor: getColorFromArea(name)}}
+                              className={"h-2 w-4 rounded-full absolute top-1 right-1 border border-white"}></span>
+                        {areaIsHuntable() ? (
+                            name
+                        ) : (
+                            <>
+                                <p className="text-xs">{name}</p>
+                                <p>{timeUntilHuntable()}</p>
+                            </>
+                        )}
+                    </div>
+                </TooltipTrigger>
+                <TooltipContent className="hidden md:block">
+                    <p>
+                        Status laatst bijgewerkt:{' '}
+                        {formatDistanceToNow(new Date(lastUpdate), {
+                            locale: nl,
+                            addSuffix: true,
+                        })}
+                    </p>
+                    <p>
+                        Laatste hunt:{' '}
+                        {lastHunt
+                            ? formatDistance(new Date(lastHunt.huntTime), new Date(), {
+                                locale: nl,
+                                addSuffix: true,
+                            })
+                            : 'Nog geen'}
+                    </p>
+                </TooltipContent>
+            </Tooltip>
+        </>
+    );
 }
 
 FoxStatus.propTypes = {
-  name: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  lastUpdate: PropTypes.string.isRequired,
-  lastHunt: PropTypes.object as PropTypes.Requireable<Hunt>,
-  hidden: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    lastUpdate: PropTypes.string.isRequired,
+    lastHunt: PropTypes.object as PropTypes.Requireable<Hunt>,
+    hidden: PropTypes.bool.isRequired,
 };
