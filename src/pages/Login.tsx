61a8fba1f78de3028090eaad47c31e2ebf9ab05f
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2025-10-15 18:08:53 +0200

    feat: New login screen + dark mode fixes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,127 +1,136 @@
-import { Input } from '../components/ui/input';
-import { Button } from '../components/ui/button';
-import logo from '@/assets/images/logo.png';
+import {Input} from '../components/ui/input';
+import {Button} from '../components/ui/button';
 
-import { Card, CardContent } from '../components/ui/card';
-import { useAuth } from '../hooks/auth.hook';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../components/ui/form';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import {Card, CardContent} from '../components/ui/card';
+import {useAuth} from '../hooks/auth.hook';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '../components/ui/form';
+import {useForm} from 'react-hook-form';
+import {useNavigate} from 'react-router-dom';
 import usePWA from 'react-pwa-install-prompt';
-import { isMobile } from 'react-device-detect';
+import {isMobile} from 'react-device-detect';
 import {useTheme} from "@/hooks/theme.hook.ts";
+import {cn} from "@/lib/utils.ts";
+import {Field, FieldDescription, FieldGroup} from "@/components/ui/field.tsx";
+import backgroundImage from '@/assets/images/background.webp';
+import {GlobeIcon} from "lucide-react";
+import {siGithub} from "simple-icons";
 
 const FormSchema = z.object({
-  email: z.string().email({ message: 'Vul een geldig e-mailadres in.' }),
-  password: z.string(),
+    email: z.string().email({message: 'Vul een geldig e-mailadres in.'}),
+    password: z.string(),
 });
 
 export default function Login() {
-  const { login } = useAuth();
-  const navigate = useNavigate();
-  const { isStandalone, isInstallPromptSupported, promptInstall } = usePWA();
+    const {login} = useAuth();
+    const navigate = useNavigate();
+    const {isStandalone, isInstallPromptSupported, promptInstall} = usePWA();
 
-  useTheme();
+    useTheme();
 
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
 
-  /**
-   * Handle the login, references the auth hook.
-   * Navigates to the dashboard if successful.
-   */
-  async function handleLogin(data: z.infer<typeof FormSchema>) {
-    const success = await login(data.email, data.password);
-    if (!success) {
-      form.setError('password', {
-        message: 'Ongeldig e-mailadres of wachtwoord.',
-      });
-      return;
+    /**
+     * Handle the login, references the auth hook.
+     * Navigates to the dashboard if successful.
+     */
+    async function handleLogin(data: z.infer<typeof FormSchema>) {
+        const success = await login(data.email, data.password);
+        if (!success) {
+            form.setError('password', {
+                message: 'Ongeldig e-mailadres of wachtwoord.',
+            });
+            return;
+        }
+
+        navigate('/');
+
+        if (isInstallPromptSupported && !isStandalone && isMobile) {
+            promptInstall();
+        }
     }
 
-    navigate('/');
-
-    if (isInstallPromptSupported && !isStandalone && isMobile) {
-      promptInstall();
-    }
-  }
-
-  return (
-    <div className="w-full lg:grid min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-4 grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <div className="flex items-center justify-center gap-3 lg:hidden mb-6">
-              <img src={logo} alt="Jotihunt Tracker" className="w-20 h-20" />
-            </div>
-            <h1 className="text-3xl font-bold">Inloggen</h1>
-            <p className=" text-muted-foreground">Vul je e-mailadres en wachtwoord in.</p>
-          </div>
-
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleLogin)}>
-              <div className="grid gap-4">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>E-mailadres</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" required placeholder="jouwnaam@scoutingscherpenzeel.nl" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Wachtwoord</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" required />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="w-full">
-                  Inloggen
-                </Button>
-              </div>
-            </form>
-          </Form>
-          <div className="mt-4 text-center text-sm">Heb je geen account? Vraag een beheerder om een account aan te maken.</div>
-        </div>
-      </div>
-      <div className="relative hidden bg-muted lg:block">
-        <div className="absolute left-1/2 -translate-x-1/2 top-12">
-          <Card>
-            <CardContent>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-3">
-                  <img src={logo} alt="Jotihunt Tracker" className="w-12 h-12" />
-                  <div>
-                    <h1 className="text-xl font-bold">Jotihunt Tracker</h1>
-                    <h2>Scouting Scherpenzeel e.o.</h2>
-                  </div>
+    return (
+        <div className="bg-muted/40 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-4xl">
+                <div className={"flex flex-col gap-6"}>
+                    <Card className="overflow-hidden p-0">
+                        <CardContent className="grid p-0 md:grid-cols-2">
+                            <Form {...form}>
+                                <form className="p-6 md:p-8" onSubmit={form.handleSubmit(handleLogin)}>
+                                    <FieldGroup>
+                                        <div className="flex flex-col items-center gap-2 text-center">
+                                            <h1 className="text-2xl font-bold">Jotihunt Tracker</h1>
+                                            <p className="text-muted-foreground text-balance">
+                                                Inloggen op je Jotihunt Tracker account
+                                            </p>
+                                        </div>
+                                        <FormField
+                                            control={form.control}
+                                            name="email"
+                                            render={({field}) => (
+                                                <FormItem>
+                                                    <FormLabel>E-mailadres</FormLabel>
+                                                    <FormControl>
+                                                        <Input {...field} type="email" required
+                                                               placeholder="jouwnaam@scoutingscherpenzeel.nl"/>
+                                                    </FormControl>
+                                                    <FormMessage/>
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="password"
+                                            render={({field}) => (
+                                                <FormItem>
+                                                    <FormLabel>Wachtwoord</FormLabel>
+                                                    <FormControl>
+                                                        <Input {...field} type="password" required/>
+                                                    </FormControl>
+                                                    <FormMessage/>
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <Field>
+                                            <Button type="submit">Login</Button>
+                                        </Field>
+                                        <FieldDescription className="text-center">
+                                            Geen account? Vraag de organisatie er een te maken!
+                                        </FieldDescription>
+                                    </FieldGroup>
+                                </form>
+                            </Form>
+                            <div className="bg-muted relative hidden md:block">
+                                <img
+                                    src={backgroundImage}
+                                    alt="Image"
+                                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.3] dark:grayscale"
+                                />
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <FieldDescription className="px-6 text-center">
+                        Ontwikkeling door Scouting Scherpenzeel e.o.<br/>
+                        <a href={"https://scoutingscherpenzeel.nl"} target={"_blank"} rel={"noreferrer"}
+                           className={cn("ml-2 underline underline-offset-2 inline-flex items-center gap-1")}>
+                            <GlobeIcon className={"h-3 w-3"}/>
+                            Website</a>
+                        <a href={"https://github.com/ScoutingScherpenzeel"} target={"_blank"} rel={"noreferrer"}
+                           className={cn("ml-2 underline underline-offset-2 inline-flex items-center gap-1")}>
+                            <span className={"fill-muted-foreground w-3 h-3"}
+                                  dangerouslySetInnerHTML={{__html: siGithub.svg}}></span>
+                            GitHub</a>
+                    </FieldDescription>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
+            </div>
         </div>
-
-        <div className="login-background w-full h-full"></div>
-      </div>
-    </div>
-  );
+    );
 }
